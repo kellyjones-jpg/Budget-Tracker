aea44349d8c968c27bc12c18fa4368268ed3734b
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2020-02-20 21:29:52 -0700

    adding changes

--- server.js
+++ server.js
@@ -20,27 +20,7 @@ mongoose.connect("mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
-// routes here
-module.exports = function(app) {
-  app.get("/api/images", function(req, res) {
-    db.Image.find({}).then(function(dbImages) {
-      res.json(dbImages);
-    });
-  });
-
-  app.get("/api/images/:id", function(req, res) {
-    db.Image.findById(req.params.id).then(function(dbImage) {
-      res.json(dbImage);
-    });
-  });
-
-
-  app.put("/api/images/:id", function(req, res) {
-    db.Image.updateOne({ _id: req.params.id }, { rating: req.body.rating }).then(function(dbImage) {
-      res.json(dbImage);
-    });
-  });
-};
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
