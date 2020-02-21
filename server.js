e253b58b7981ed01647717fdf12512be2d0b0ae3
Kelly Jones <54192972+kellyjones-jpg@users.noreply.github.com>
2020-02-20 19:30:26 -0700

    Update server.js

--- server.js
+++ server.js
@@ -21,6 +21,26 @@ mongoose.connect("mongodb://localhost/budget", {
 });
 
 // routes here
+module.exports = function(app) {
+  app.get("/api/images", function(req, res) {
+    db.Image.find({}).then(function(dbImages) {
+      res.json(dbImages);
+    });
+  });
+
+  app.get("/api/images/:id", function(req, res) {
+    db.Image.findById(req.params.id).then(function(dbImage) {
+      res.json(dbImage);
+    });
+  });
+
+
+  app.put("/api/images/:id", function(req, res) {
+    db.Image.updateOne({ _id: req.params.id }, { rating: req.body.rating }).then(function(dbImage) {
+      res.json(dbImage);
+    });
+  });
+};
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
